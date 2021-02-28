8d298f3952cf19e65781c390a92c5b96cf89cec9
Roopa <roopa.grsv@gmail.com>
2021-02-28 18:35:18 +0530

    commit

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name  = "rg1_storage"
     storage_account_name = "grsvstorageaccount"
     container_name       = "containergrsv"
-    key                  = "prod13.terraform.tfstate"
+    key                  = "terraform.tfstate"
     access_key           = "TFsU9Xd1Bsvh6NMwToaOFFF+wqunmiHctyaTLYHHmg6QKfwESISEhApkKRSD2nIabEINr6kWdcbHp7hgTsPpcw=="
   }
 }
