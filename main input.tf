fc5c9dade5f40c6943f0959d07f6682e71d77d60
Roopa <roopa.grsv@gmail.com>
2021-02-28 18:13:35 +0530

    commit

--- main input.tf
+++ main input.tf
@@ -1,24 +1,24 @@
 resource "azurerm_resource_group" "example" {
-  name     = "NICRG1000"
+  name     = "NICRG1001"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "nicvnetwork12"
+  name                = "nicvnetwork13"
   address_space       = ["192.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "devsub12"
+  name                 = "devsub13"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["192.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "NIC12"
+  name                = "NIC13"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
